1b12867bbe0ba213e6cb4345e91234ac2488dc0e
cdemi <christopher.demicoli@outlook.com>
2017-08-23 18:50:44 +0200

    added template to repo

---
+++ .template/bride/lib/custom-slider/home.js
@@ -0,0 +1,24 @@
+(function ($) {
+ "use strict";
+    
+		//---------------------------------------------
+		//Nivo slider
+		//---------------------------------------------
+			 $('#nivoslider').nivoSlider({
+				effect: 'random',
+				slices: 15,
+				boxCols: 8,
+				boxRows: 4,
+				animSpeed: 500,
+				pauseTime: 5000,
+				startSlide: 0,
+				directionNav: true,
+				controlNavThumbs: false,
+				pauseOnHover: false,
+				manualAdvance: true,
+			 });
+			  
+           
+    
+			 
+})(jQuery); 
