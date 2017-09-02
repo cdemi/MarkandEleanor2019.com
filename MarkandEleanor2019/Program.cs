cb28f0f978abf23635ebc1b03e47f6b6a3ce73de
cdemi <christopher.demicoli@outlook.com>
2017-09-03 01:01:02 +0200

    This resolves #13

--- MarkandEleanor2019/Program.cs
+++ MarkandEleanor2019/Program.cs
@@ -19,6 +19,7 @@ namespace MarkandEleanor2019
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
