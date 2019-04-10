6710eed49fdb08df39e9a074e72c22dd4d0e7dfe
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-04-10 23:13:44 +0200

    Added offline page

--- MarkandEleanor2019/wwwroot/pwabuilder-sw.js
+++ MarkandEleanor2019/wwwroot/pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = "pwabuilder-offline";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html";
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
