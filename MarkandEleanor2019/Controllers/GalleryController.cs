16aa1051e746b2e988ee714ef087a90ebd101512
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-04-16 11:52:09 +0200

    This closes #31

--- MarkandEleanor2019/Controllers/GalleryController.cs
+++ MarkandEleanor2019/Controllers/GalleryController.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace MarkandEleanor2019.Controllers
 {
     public class GalleryController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public GalleryController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Photos(string password)
+        {
+            if (password != null && password.Equals(configuration.GetValue<string>("GalleryPassword")))
+                return View();
+            else
+                return RedirectToAction("Index");
         }
     }
 }
